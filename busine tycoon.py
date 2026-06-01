e86e33f6b9871ad515e398119399b85491836918
Codingeverythin <ahmetzehra11@gmail.com>
2026-06-01 20:53:46 +0300

    Update busine tycoon.py

--- busine tycoon.py
+++ busine tycoon.py
@@ -1,7 +1,8 @@
+<style>
 import streamlit as st
 st.markdown("""
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Share+Tech+Mono&family=VT323&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Share+Tech+Mono&family=VT323&display=swap');
 
   :root{--g:#00ff41;--gd:#00aa2b;--gf:#003d0f;--gm:#00cc35;--amber:#ffb300;--red:#ff4444;--bg:#0a0f0a;--blue:#00cfff;--purple:#cc44ff;}
   *{box-sizing:border-box;margin:0;padding:0;}
